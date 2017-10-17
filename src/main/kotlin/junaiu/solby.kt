1f847bbf5c71360bbd62f5a60ae82c69cfc7d273
junhuuuu <junhuuuu@hjm.local>
2017-10-17 20:51:18 +0800

    update pom

--- src/main/kotlin/junaiu/solby.kt
+++ src/main/kotlin/junaiu/solby.kt
@@ -1,14 +1,8 @@
 package junaiu
 
-import org.jetbrains.ktor.application.ApplicationCallPipeline
-import org.jetbrains.ktor.application.call
 import org.jetbrains.ktor.host.embeddedServer
-import org.jetbrains.ktor.http.ContentType
-import org.jetbrains.ktor.logging.CallLogging
-import org.jetbrains.ktor.logging.CallLogging.Feature.install
 import org.jetbrains.ktor.netty.Netty
-import org.jetbrains.ktor.request.uri
-import org.jetbrains.ktor.response.header
+import org.jetbrains.ktor.response.respond
 import org.jetbrains.ktor.response.respondText
 import org.jetbrains.ktor.routing.get
 import org.jetbrains.ktor.routing.routing
