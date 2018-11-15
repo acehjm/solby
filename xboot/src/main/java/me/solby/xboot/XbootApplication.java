1eb4b83edea69b301c6c9b5a3cb216fafc22acc5
ameth <majhdkl@gmail.com>
2018-11-15 13:56:08 +0800

    recode

---
+++ xboot/src/main/java/me/solby/xboot/XbootApplication.java
@@ -0,0 +1,12 @@
+package me.solby.xboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XbootApplication.class, args);
+    }
+}
---
+++ xboot/src/test/java/me/solby/xboot/XbootApplicationTests.java
@@ -0,0 +1,16 @@
+package me.solby.xboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
