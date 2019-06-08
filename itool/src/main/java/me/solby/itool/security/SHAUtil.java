dbcb5d66fb12586533df1261e0c8f2c92b0e98b4
ameth <majhdkl@gmail.com>
2019-06-08 19:05:29 +0800

    add SHAUtil & Hex

--- itool/src/main/java/me/solby/itool/security/SHAUtil.java
+++ itool/src/main/java/me/solby/itool/security/SHAUtil.java
@@ -1,10 +1,90 @@
 package me.solby.itool.security;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
- * me.solby.itool.security
+ * JDK MessageDigest 实现
  *
  * @author majhdk
  * @date 2019-05-25
  */
 public class SHAUtil {
+
+    private SHAUtil() {
+    }
+
+    /**
+     * 字符串做SHA256摘要,输出Base64
+     *
+     * @param data
+     * @return
+     */
+    public static String sha256HexBase64(String data) {
+        byte[] bytes = sha256Hex(data.getBytes(StandardCharsets.UTF_8));
+        return new String(Base64Codec.encode(bytes), StandardCharsets.UTF_8);
+    }
+
+    /**
+     * 字符串做SHA256摘要
+     *
+     * @param data
+     * @return
+     */
+    public static String sha256Hex(String data) {
+        byte[] bytes = sha256Hex(data.getBytes(StandardCharsets.UTF_8));
+        return Hex.encodeHexString(bytes);
+    }
+
+    /**
+     * 字节数组做SHA256摘要
+     *
+     * @param bytes
+     * @return
+     */
+    public static byte[] sha256Hex(byte[] bytes) {
+        return shaAlgorithmHex(bytes, "SHA-256");
+    }
+
+    /**
+     * 自定义算法摘要
+     *
+     * @param data
+     * @param algorithm
+     * @return
+     */
+    public static String shaAlgorithmHex(String data, String algorithm) {
+        byte[] bytes = shaAlgorithmHex(data.getBytes(StandardCharsets.UTF_8), algorithm);
+        return Hex.encodeHexString(bytes);
+    }
+
+    /**
+     * 自定义算法摘要,返回Base64
+     *
+     * @param data
+     * @param algorithm
+     * @return
+     */
+    public static String shaAlgorithmHexBase64(String data, String algorithm) {
+        byte[] bytes = shaAlgorithmHex(data.getBytes(StandardCharsets.UTF_8), algorithm);
+        return new String(Base64Codec.encode(bytes), StandardCharsets.UTF_8);
+    }
+
+    /**
+     * 自定义算法摘要
+     *
+     * @param bytes
+     * @param algorithm
+     * @return
+     */
+    public static byte[] shaAlgorithmHex(byte[] bytes, String algorithm) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            return digest.digest(bytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new SecurityException(algorithm + " hash failure", e);
+        }
+    }
+
 }
---
+++ itool/src/test/java/me/solby/itool/security/SHAUtilTest.java
@@ -0,0 +1,28 @@
+package me.solby.itool.security;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * me.solby.itool.security
+ *
+ * @author majhdk
+ * @date 2019-06-08
+ */
+class SHAUtilTest {
+
+    @Test
+    void sha256HexWithString() {
+        String data = "SHA-256";
+        System.out.println(SHAUtil.sha256Hex(data));
+        System.out.println(SHAUtil.sha256HexBase64(data));
+    }
+
+    @Test
+    void sha256HexWithBytes() {
+    }
+
+    @Test
+    void shaAlgorithmHex() {
+        System.out.println(SHAUtil.shaAlgorithmHex("SHA-256", "MD5"));
+    }
+}
