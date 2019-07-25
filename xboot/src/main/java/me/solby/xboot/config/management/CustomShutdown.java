c9b0f935a627e26ce67e60cd86a42b77195c7b34
ameth <majhdkl@gmail.com>
2019-07-26 00:48:08 +0800

    add custom shutdown config

---
+++ xboot/src/main/java/me/solby/xboot/config/management/CustomShutdown.java
@@ -0,0 +1,83 @@
+package me.solby.xboot.config.management;
+
+import org.apache.catalina.connector.Connector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 自定义容器关闭时行为
+ * 1。一般配置为等待线程处理完成后关闭
+ * 2。在超时时间内未处理完，则强制关闭
+ *
+ * @author majhdk
+ * @date 2019-07-26
+ */
+@Component
+public class CustomShutdown implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomShutdown.class);
+
+    /**
+     * 线程超时时间
+     */
+    private static final int TIMEOUT = 30;
+
+    /**
+     * 连接器
+     */
+    private volatile Connector connector;
+
+    @Override
+    public void onApplicationEvent(ContextClosedEvent event) {
+        this.connector.pause();
+        var executor = this.connector.getProtocolHandler().getExecutor();
+        if (executor instanceof ThreadPoolExecutor) {
+            try {
+                logger.warn("Application ready to close");
+                var threadPoolExecutor = (ThreadPoolExecutor) executor;
+                threadPoolExecutor.shutdown();
+                if (!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+                    logger.warn("The application waits for " + TIMEOUT + " seconds and will force a shutdown");
+                    threadPoolExecutor.shutdownNow();
+                    if (!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+                        logger.error("Application shutdown failed");
+                    }
+                }
+                if (threadPoolExecutor.isShutdown()) {
+                    logger.warn("Application has been closed");
+                }
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    @Override
+    public void customize(Connector connector) {
+        this.connector = connector;
+    }
+
+    /**
+     * 将自定义Connector添加到内置Tomcat容器
+     *
+     * @param customShutdown
+     * @return
+     */
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory(final CustomShutdown customShutdown) {
+        var tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addConnectorCustomizers(customShutdown);
+        return tomcatServletWebServerFactory;
+    }
+}
+
---
+++ xboot/src/main/java/me/solby/xboot/controller/BootController.java
@@ -0,0 +1,28 @@
+package me.solby.xboot.controller;
+
+import me.solby.itool.response.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * me.solby.xboot.controller
+ *
+ * @author majhdk
+ * @date 2019-07-26
+ */
+@RestController
+public class BootController {
+
+    @GetMapping("/runner")
+    public Result<String> running() {
+        System.out.println("开始运行");
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("运行结束");
+        return new Result<>();
+    }
+
+}
