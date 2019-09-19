6be3f3acf224a965a045c8a460c5e37efd8f963d
ameth <majhdkl@gmail.com>
2019-09-20 00:43:09 +0800

    remove component scan

--- xboot/src/main/java/me/solby/xboot/XbootApplication.java
+++ xboot/src/main/java/me/solby/xboot/XbootApplication.java
@@ -2,16 +2,9 @@ package me.solby.xboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@ComponentScans({
-        @ComponentScan("me.solby.xconfig.*"),
-        @ComponentScan("me.solby.xquartz.*"),
-        @ComponentScan("me.solby.xoauth.*"),
-})
 @EnableAspectJAutoProxy
 public class XbootApplication {
 
